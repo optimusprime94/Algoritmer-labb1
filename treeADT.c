baf14c7422fc103d580ec5e285300867bb6db2d3
optimusprime94 <dzekson94@gmail.com>
2016-05-02 10:34:59 +0200

    Added files via upload

---
+++ treeADT.c
@@ -0,0 +1,11 @@
+#include "genlib.h"
+#include "treeADT.h"
+
+
+void NewTree(string type) {}
+
+void FreeTree(void) {}
+
+void TreeInsert(void) {}
+
+void TreePrint(void) {}
---
+++ treeADT.h
@@ -0,0 +1,9 @@
+#ifndef _treeADT_h
+#define _treeADT_h
+
+void NewTree(string type);
+void FreeTree(void);
+void TreeInsert(void);
+void TreePrint(void);
+
+#endif // !_2-3treeADT.h_
